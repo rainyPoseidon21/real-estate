ab0495ebace34fe23b241386f9de5134a17ee36d
mmazlah <mmazlah@guidewire.com>
2024-01-18 18:53:09 +0800

    React router dom & auth pages draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}>App</Route>
+        <Route path="/sign-in" element={<SignIn/>}>App</Route>
+        <Route path="/sign-up" element={<SignUp/>}>App</Route>
+        <Route path="/about" element={<About/>}>App</Route>
+        <Route path="/profile" element={<Profile/>}>App</Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
