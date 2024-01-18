96ee08cdd9269884b397eb7f45899406c6cebf0d
mmazlah <mmazlah@guidewire.com>
2024-01-18 23:58:40 +0800

    Adding mongoDB config

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
-app.listen(3000, () => {
+app.listen(3300, () => {
   console.log("Server running on port 3000!!");
 });
