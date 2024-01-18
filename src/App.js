cdf85dbe97fffcfeee7bfc9a51ed356c5c3bb89f
mmazlah <mmazlah@guidewire.com>
2024-01-18 19:28:47 +0800

    Adding header section details

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
-const App = () => {
+export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
-        <Route path="/" element={<Home/>}>App</Route>
-        <Route path="/sign-in" element={<SignIn/>}>App</Route>
-        <Route path="/sign-up" element={<SignUp/>}>App</Route>
-        <Route path="/about" element={<About/>}>App</Route>
-        <Route path="/profile" element={<Profile/>}>App</Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
-};
-
-export default App;
+}
