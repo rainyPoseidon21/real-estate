3cafe02d491daa063b43c3a4b5f1d623ebc99eed
rainyPoseidon21 <muhaimiemazlah0116346@gmail.com>
2024-01-19 23:12:12 +0800

    Adding ui details for sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <div className="flex flex-col max-w-lg mx-auto">
+        <form className="flex flex-col gap-4 p-3">
+          <input
+            type="text"
+            placeholder="username"
+            id="username"
+            className="border p-3 rounded-lg"
+          />
+          <input
+            type="text"
+            placeholder="email"
+            id="email"
+            className="border p-3 rounded-lg"
+          />
+          <input
+            type="text"
+            placeholder="password"
+            id="password"
+            className="border p-3 rounded-lg"
+          />
+          <button
+            className="bg-slate-700 text-white p-3
+         rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            SIGN UP
+          </button>
+        </form>
+        <div className="flex gap-2 mt-5 px-3">
+          <p>Have an account?</p>
+          <Link to={"/signin"}>
+            <span className="text-blue-700">Sign in</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
